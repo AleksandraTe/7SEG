c5161d1d99bfde55fdffc02a45c7f3ffe655bad1
Aleksandra Te <aleksandra.tenderenda@gmail.com>
2020-04-15 20:38:01 +0200

    test

---
+++ TEST/7_SEGtest/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace _7_SEGtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this._7segment1 = new _7_SEG._7segment();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(294, 271);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Wyświetl";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // _7segment1
+            // 
+            this._7segment1.ColOff = System.Drawing.Color.DarkGray;
+            this._7segment1.ColOn = System.Drawing.Color.Red;
+            this._7segment1.Location = new System.Drawing.Point(171, 102);
+            this._7segment1.Name = "_7segment1";
+            this._7segment1.Nr = "0";
+            this._7segment1.NumberOfDisplays = 5;
+            this._7segment1.Size = new System.Drawing.Size(606, 150);
+            this._7segment1.TabIndex = 3;
+            this._7segment1.Load += new System.EventHandler(this._7segment1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 356);
+            this.Controls.Add(this._7segment1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private _7_SEG._7segment _7segment1;
+    }
+}
+
---
+++ TEST/7_SEGtest/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_SEGtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           
+            InitializeComponent();
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _7segment1.Nr = textBox1.Text;
+            _7segment1.Invalidate();
+        }
+
+        private void _7segment1_Load(object sender, EventArgs e)
+        {
+            textBox1.MaxLength = _7segment1.NumberOfDisplays;
+        }
+
+        
+    }
+}
